1a1ee0b1ba91189450cf0aa69c26be42108ebc7b
fieliel-star <fieliel369@gmail.com>
2026-02-08 02:22:48 +0700

    71251214

--- latuhanMandiriNo3/mandiri3.py
+++ latuhanMandiriNo3/mandiri3.py
@@ -3,5 +3,4 @@ modalAwal = 200000000
 targetUang = 400000000
 bungaPerTahun = 0.1
 waktuYangDiButuhkan = math.log(targetUang / modalAwal) / math.log(1 + bungaPerTahun)
-
 print ("Waktu yang dibutuhkan =", round(waktuYangDiButuhkan), "Tahun")
